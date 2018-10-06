e51e471b413737e59b776119ea71c67e692458f0
Seimizu Joukan <saimizi@gmail.com>
2018-10-07 00:43:06 +0900

    Update libtrace.

--- bconf/package/libtrace/libtrace.mk
+++ bconf/package/libtrace/libtrace.mk
@@ -1,5 +1,5 @@
 LIBTRACE_VERSION = master
-LIBTRACE_SITE = https://github.com/saimizi/libtrace.git
+LIBTRACE_SITE = https://github.com/saimizi/trial-works
 LIBTRACE_SITE_METHOD = git
 LIBTRACE_INSTALL_STAGING = YES
 
@@ -8,21 +8,21 @@ LIBTRACE_POST_EXTRACT_HOOKS += GIT_PULL_TO_CURRENT
 endif
 
 define LIBTRACE_BUILD_CMDS
-	 $(MAKE) CC=$(TARGET_CC) -j1 -C $(@D)
+	 $(MAKE) CC=$(TARGET_CC) -j1 -C $(@D)/libtrace
 endef
 
 define LIBTRACE_INSTALL_STAGING_CMDS
 	install -d $(STAGING_DIR)/usr/lib/
-	cp -d $(@D)/libtrace.so* $(STAGING_DIR)/usr/lib/
+	cp -d $(@D)/libtrace/libtrace.so* $(STAGING_DIR)/usr/lib/
 	install -d $(STAGING_DIR)/usr/include/
-	install -m 0644 $(@D)/libtrace.h $(STAGING_DIR)/usr/include
+	install -m 0644 $(@D)/libtrace/libtrace.h $(STAGING_DIR)/usr/include
 endef
 
 define LIBTRACE_INSTALL_TARGET_CMDS
 	install -d $(TARGET_DIR)/usr/lib/
-	cp -d $(@D)/libtrace.so* $(TARGET_DIR)/usr/lib/
+	cp -d $(@D)/libtrace/libtrace.so* $(TARGET_DIR)/usr/lib/
 	install -d $(TARGET_DIR)/usr/bin/
-	install -m 0755 $(@D)/test/trace_test $(TARGET_DIR)/usr/bin/
+	install -m 0755 $(@D)/libtrace/test/trace_test $(TARGET_DIR)/usr/bin/
 endef
 
 
